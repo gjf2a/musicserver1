8ab865fa036ffff1985a89e789b123c406c39c28
gjf2a <ferrer@hendrix.edu>
2022-10-06 12:15:15 -0500

    Fixed ADSR disparity bug. Fixed and Live ADSR now sound the same, except for the issue of the release time.

--- src/adsr.rs
+++ src/adsr.rs
@@ -3,9 +3,22 @@ use std::fmt::Debug;
 use std::sync::Arc;
 use crossbeam_utils::atomic::AtomicCell;
 
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+pub enum SoundMsg {
+    Play, Release, Finished
+}
+
 /// Credit: https://stackoverflow.com/a/73205224/906268 for showing how to write the return type
 pub fn adsr_live(attack: f64, decay: f64, sustain: f64, release: f64, note_m: Arc<AtomicCell<SoundMsg>>) -> An<Envelope<f64, f64, impl Fn(f64)->f64 + 'static, f64>> {
-    let adsr = AtomicCell::new(Adsr::live(attack, decay, sustain, release));
+    adsr(attack, decay, sustain, release, None, note_m)
+}
+
+pub fn adsr_fixed(attack: f64, decay: f64, sustain_time: f64, sustain_level: f64, release: f64, note_m: Arc<AtomicCell<SoundMsg>>) -> An<Envelope<f64, f64, impl Fn(f64)->f64 + 'static, f64>> {
+    adsr(attack, decay, sustain_level, release, Some(attack + decay + sustain_time), note_m)
+}
+
+fn adsr(attack: f64, decay: f64, sustain: f64, release: f64, release_start: Option<f64>, note_m: Arc<AtomicCell<SoundMsg>>) -> An<Envelope<f64, f64, impl Fn(f64)->f64 + 'static, f64>> {
+    let adsr = AtomicCell::new(Adsr {attack, decay, sustain, release, release_start});
     lfo(move |t| {
         if note_m.load() == SoundMsg::Release {
             let mut adsr_inner = adsr.load();
@@ -20,58 +33,41 @@ pub fn adsr_live(attack: f64, decay: f64, sustain: f64, release: f64, note_m: Ar
     })
 }
 
-pub fn adsr_fixed(attack: f64, decay: f64, sustain_time: f64, sustain_level: f64, release: f64, note_m: Arc<AtomicCell<SoundMsg>>) -> An<Envelope<f64, f64, impl Fn(f64)->f64 + 'static, f64>> {
-    let adsr = AtomicCell::new(Adsr::fixed(attack, decay, sustain_time, sustain_level, release));
-    lfo(move |t| {
-        match adsr.load().volume(t) {
-            Some(v) => v,
-            None => {note_m.store(SoundMsg::Finished); 0.0}
-        }
-    })
-}
-
-#[derive(Copy, Clone, Debug, Eq, PartialEq)]
-pub enum SoundMsg {
-    Play, Release, Finished
-}
-
 #[derive(Copy, Clone, Debug)]
-pub struct Adsr {
+struct Adsr {
     attack: f64, decay: f64, sustain: f64, release: f64, release_start: Option<f64>
 }
 
 impl Adsr {
-    pub fn live(attack: f64, decay: f64, sustain: f64, release: f64) -> Self {
-        Adsr {attack, decay, sustain, release, release_start: None}
-    }
-
-    pub fn fixed(attack: f64, decay: f64, sustain_time: f64, sustain_level: f64, release: f64) -> Self {
-        Adsr {attack, decay, sustain: sustain_level, release, release_start: Some(attack + decay + sustain_time)}
-    }
-
-    pub fn release(&mut self, time_now: f64) {
+    fn release(&mut self, time_now: f64) {
         self.release_start = Some(time_now);
     }
 
-    pub fn volume(&self, time: f64) -> Option<f64> {
+    fn volume(&self, time: f64) -> Option<f64> {
         match self.release_start {
-            None => {
-                if time < self.attack {
-                    Some(lerp(0.0, 1.0, time / self.attack))
-                } else if time - self.attack < self.decay {
-                    Some(lerp(1.0, self.sustain, (time - self.attack) / self.decay))
-                } else {
-                    Some(self.sustain)
-                }
-            }
+            None => Some(self.ads(time)),
             Some(release_start) => {
-                let release_time = time - release_start;
-                if release_time > self.release {
-                    None
+                if time < release_start {
+                    Some(self.ads(time))
                 } else {
-                    Some(lerp(self.sustain, 0.0, release_time / self.release))
+                    let release_time = time - release_start;
+                    if release_time > self.release {
+                        None
+                    } else {
+                        Some(lerp(self.sustain, 0.0, release_time / self.release))
+                    }
                 }
             }
+        }
+    }
+
+    fn ads(&self, time: f64) -> f64 {
+        if time < self.attack {
+            lerp(0.0, 1.0, time / self.attack)
+        } else if time - self.attack < self.decay {
+            lerp(1.0, self.sustain, (time - self.attack) / self.decay)
+        } else {
+            self.sustain
         }
     }
 }
