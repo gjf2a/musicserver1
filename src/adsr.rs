aca012c72ebe38af33c4292053bad17e81ab4370
gjf2a <ferrer@hendrix.edu>
2022-10-06 05:17:20 -0500

    In ADSR, SoundMsg::Stop is now unambiguously a command to the thread running the sound, not to the ADSR. Only SoundMsg::Release is a command to the ADSR.

--- src/adsr.rs
+++ src/adsr.rs
@@ -7,9 +7,6 @@ use crossbeam_utils::atomic::AtomicCell;
 pub fn adsr_live(attack: f64, decay: f64, sustain: f64, release: f64, note_m: Arc<AtomicCell<SoundMsg>>) -> An<Envelope<f64, f64, impl Fn(f64)->f64 + 'static, f64>> {
     let adsr = AtomicCell::new(Adsr::live(attack, decay, sustain, release));
     lfo(move |t| {
-        if note_m.load() == SoundMsg::Stop {
-            return 0.0;
-        }
         if note_m.load() == SoundMsg::Release {
             let mut adsr_inner = adsr.load();
             adsr_inner.release(t);
