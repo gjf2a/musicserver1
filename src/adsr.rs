193e626d403d01d13e445838e78d5368eed12763
gjf2a <ferrer@hendrix.edu>
2022-10-05 20:57:04 -0500

    Eliminated the fixed ADSR - I'm not really going to use it. Observed that a light quick keytough on the keyboard produces an unending sound - need to figure out why.

--- src/adsr.rs
+++ src/adsr.rs
@@ -4,6 +4,7 @@ use std::fmt::{Debug, Formatter};
 use std::sync::Arc;
 use crossbeam_utils::atomic::AtomicCell;
 
+/// Credit: https://stackoverflow.com/a/73205224/906268 for showing how to write the return type
 pub fn adsr_live(attack: f64, decay: f64, sustain: f64, release: f64, note_m: Arc<AtomicCell<SoundMsg>>) -> An<Envelope<f64, f64, impl Fn(f64)->f64 + 'static, f64>> {
     let adsr = AtomicCell::new(Adsr::new(attack, decay, sustain, release));
     lfo(move |t| {
@@ -21,31 +22,6 @@ pub fn adsr_live(attack: f64, decay: f64, sustain: f64, release: f64, note_m: Ar
             None => {note_m.store(SoundMsg::Stop); 0.0}
         }
     })
-}
-
-/// Credit: https://stackoverflow.com/a/73205224/906268 for showing how to write the return type
-pub fn adsr_fixed(attack: f64, decay: f64, sustain: f64, sustain_level: f64, release: f64) -> An<Envelope<f64, f64, impl Fn(f64)->f64 + 'static, f64>> {
-    lfo(move |time_s| adsr_level(time_s, attack, decay, sustain, sustain_level, release))
-}
-
-pub fn adsr_level(time_s: f64, attack: f64, decay: f64, sustain_time: f64, sustain_level: f64, release: f64) -> f64 {
-    let mut time_s = time_s;
-    if time_s < attack {
-        return lerp(0.0, 1.0, time_s / attack);
-    }
-    time_s -= attack;
-    if time_s < decay {
-        return lerp(1.0, sustain_level, time_s / decay);
-    }
-    time_s -= decay;
-    if time_s < sustain_time {
-        return sustain_level;
-    }
-    time_s -= release;
-    if time_s < release {
-        return lerp(sustain_level, 0.0, time_s / release);
-    }
-    0.0
 }
 
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
--- src/synth_sounds.rs
+++ src/synth_sounds.rs
@@ -1,4 +1,4 @@
-use fundsp::hacker::{lerp11, envelope, midi_hz, pulse, sin_hz, triangle};
+use fundsp::hacker::{lerp11, envelope, pulse, sin_hz, triangle};
 use fundsp::prelude::AudioUnit64;
 use std::sync::Arc;
 use crate::{adsr_live, SoundMsg, ChooserTable, SynthFuncType, SynthTable, convert_midi, arc_vec};
@@ -6,70 +6,17 @@ use crossbeam_utils::atomic::AtomicCell;
 
 pub fn make_synth_table() -> SynthTable {
     let synth_funcs: Vec<(&str, Arc<SynthFuncType>)> = arc_vec![
-        ("ADSR Pulse", adsr_pulse),
-        ("ADSR Triangle", adsr_tri)];
+        ("Live ADSR Pulse", adsr_live_pulse),
+        ("Live ADSR Triangle", adsr_live_tri)];
     ChooserTable::from(&synth_funcs)
 }
 
-pub fn adsr_tri(pitch: u8, volume: u8, note_m: Arc<AtomicCell<SoundMsg>>) -> Box<dyn AudioUnit64> {
+pub fn adsr_live_tri(pitch: u8, volume: u8, note_m: Arc<AtomicCell<SoundMsg>>) -> Box<dyn AudioUnit64> {
     let (pitch, volume) = convert_midi(pitch, volume);
     Box::new(envelope(move |_t| pitch) >> triangle() * adsr_live(0.2, 0.2, 0.4, 0.2, note_m) * volume)
 }
 
-pub fn adsr_pulse(pitch: u8, volume: u8, note_m: Arc<AtomicCell<SoundMsg>>) -> Box<dyn AudioUnit64> {
+pub fn adsr_live_pulse(pitch: u8, volume: u8, note_m: Arc<AtomicCell<SoundMsg>>) -> Box<dyn AudioUnit64> {
     let (pitch, volume) = convert_midi(pitch, volume);
     Box::new(envelope(move |t| (pitch, lerp11(0.01, 0.99, sin_hz(0.05, t)))) >> pulse() * adsr_live(0.2, 0.2, 0.4, 0.2, note_m) * volume)
 }
-
-/*
-pub fn make_synth_table() -> SynthTable {
-    let synth_funcs = func_vec![SynthFunc,
-            ("Sine Pulse", sine_pulse),
-            ("ADSR 1", expr1),
-            ("ADSR 2", expr2),
-            ("Simple Triangle", simple_tri)];
-    SynthTable::from(&synth_funcs)
-}
-
-pub fn sine_pulse(pitch: u8, volume: u8, notes_in_use: Arc<DashMap<u8,NoteStatus>>) -> Box<dyn AudioUnit64> {
-    let volume = if let Some(status) = notes_in_use.get(&pitch) {
-        if let NoteStatus::Release(inst) = status.value() {
-            notes_in_use.remove(&pitch);
-        }
-        0.0
-    } else {
-        velocity2volume(volume.into())
-    };
-    let pitch = midi_hz(pitch as f64);
-    Box::new(lfo(move |t| {
-        (pitch, lerp11(0.01, 0.99, sin_hz(0.05, t)))
-    }) >> pulse() * volume)
-}
-
-pub fn simple_tri(pitch: u8, volume: u8, notes_in_use: Arc<DashMap<u8,NoteStatus>>) -> Box<dyn AudioUnit64> {
-    let volume = if let Some(status) = notes_in_use.get(&pitch) {
-        if let NoteStatus::Release(inst) = status.value() {
-            notes_in_use.remove(&pitch);
-        }
-        0.0
-    } else {
-        velocity2volume(volume.into())
-    };
-    let pitch = midi_hz(pitch as f64);
-    Box::new(lfo(move |_t| pitch) >> triangle() * volume)
-}
-
-pub fn expr1(pitch: u8, volume: u8, releasing: Arc<DashMap<u8,NoteStatus>>) -> Box<dyn AudioUnit64> {
-    releasing.remove(&pitch);
-    let pitch = midi_hz(pitch as f64);
-    let volume = velocity2volume(volume.into());
-    Box::new(lfo(move |_t| pitch) >> triangle() * adsr_fixed(0.2, 0.2, 0.4, 0.4, 0.2) * volume)
-}
-
-pub fn expr2(pitch: u8, volume: u8, releasing: Arc<DashMap<u8,NoteStatus>>) -> Box<dyn AudioUnit64> {
-    releasing.remove(&pitch);
-    let pitch = midi_hz(pitch as f64);
-    let volume = velocity2volume(volume.into());
-    Box::new(lfo(move |t| {(pitch, lerp11(0.01, 0.99, sin_hz(0.05, t)))}) >> pulse() * adsr_fixed(0.2, 0.2, 0.4, 0.4, 0.2) * volume)
-}
-*/
